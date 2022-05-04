38e40e750b31b261b7c572a95109bef745632adc
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-05-04 16:28:33 -0300

    📦 refactor: change login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -14,27 +14,36 @@ export default function Login() {
   };
 
   return (
-    <div className="login">
-      <form className="loginForm">
-        <input
-          type="text"
-          placeholder="email"
-          className="loginInput"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="loginInput"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          className="loginButton"
-          onClick={handleLogin}
-          disabled={isFetching}
-        >
-          Login
-        </button>
+    <div className="main">
+      <form id="login_form" class="form_class">
+        <div class="form_div">
+          <label>Login:</label>
+          <input
+            class="field_class"
+            name="login_txt"
+            type="text"
+            placeholder="Email"
+            autofocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password:</label>
+          <input
+            id="pass"
+            class="field_class"
+            name="password_txt"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            class="submit_class"
+            type="submit"
+            onClick={handleLogin}
+            disabled={isFetching}
+          >
+            Login
+          </button>
+        </div>
       </form>
     </div>
   );
